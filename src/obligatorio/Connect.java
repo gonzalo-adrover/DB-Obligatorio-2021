dd308acf9b3699ccf88d6e398c80f140e26d876c
Gonzalo Adrover <adrovergonzalo@gmail.com>
2021-11-09 21:09:08 -0300

    Initial commit

---
+++ src/obligatorio/Connect.java
@@ -0,0 +1,33 @@
+package obligatorio;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connect {
+//    Cambiar solamente user, pass e IP del string URL de ser necesario
+    private static Connection conn;
+    private static final String driver = "org.postgresql.Driver";
+    private static final String user = "postgres";
+    private static final String pass = "1234";
+    private static final String db = "Obligatorio";
+    private static final String url = "jdbc:postgresql://192.168.56.104:5432/"+db+"";
+    
+    public Connect(){
+        conn = null;
+        try{
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, user, pass);
+            
+            // Connect?
+            if(conn != null)
+                System.out.println("Conexión establecida exitosamente");
+        }catch (ClassNotFoundException | SQLException ex){
+            System.out.println("Conexión Fallida:\n\n"+ex);
+        }
+    }
+    
+    public Connection getConnection(){
+        return conn;
+    }
+}
